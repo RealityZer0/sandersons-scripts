9ec1e370eb7faa4d3196c78aabdd32236bf7452a
Scott <realyze74@gmail.com>
2017-10-25 16:27:54 -0700

    FixPerms to OPP

--- bash/fixUserPerms.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/usr/bin/find ~ $TMPDIR.. -exec chflags -h nouchg,nouappnd,noschg,nosappnd {} + \
--exec chown -h $UID {} + -exec chmod +rw {} + \
--exec chmod -h -N {} + -type d \
--exec chmod -h +x {} + 2>&-
