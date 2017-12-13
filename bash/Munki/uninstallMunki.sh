c1b8bb15ebfce54d525dcde2b5889b61a6b5249d
Scott Anderson <sa8678@yp.com>
2017-12-13 07:58:25 -0800

    Updat munkiuninstall script

--- bash/Munki/uninstallMunki.sh
+++ bash/Munki/uninstallMunki.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
-sudo launchctl unload /Library/LaunchDaemons/com.googlecode.munki.*
+#==========#
+# ABOUT THIS SCRIPT:
+# NAME: Uninstall Munki
+# SYNOPSIS: Uninstall all files/folders involving Munki.
+#==========#
+# HISTORY:
+# Version 1.0
+# Created by Scott Anderson on 12/12/17
+# Revision Date
+#==========#
+# PATH
+#==========#
+PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki
+export PATH
+#==========#
+# VARIABLES
+#==========#
+TARGET_VOL="$3"
+#==========#
+# COMMANDS
+#==========#
+launchctl unload /Library/LaunchDaemons/com.googlecode.munki.*
 
-sudo rm -rf "/Applications/Utilities/Managed Software Update.app"
+rm -rf "/Applications/Utilities/Managed Software Update.app"
 #Munki 2 only:
-sudo rm -rf "/Applications/Managed Software Center.app"
+rm -rf "/Applications/Managed Software Center.app"
 
-sudo rm -f /Library/LaunchDaemons/com.googlecode.munki.*
-sudo rm -f /Library/LaunchAgents/com.googlecode.munki.*
-sudo rm -rf "/Library/Managed Installs"
-sudo rm -rf /usr/local/munki
-sudo rm /etc/paths.d/munki
-sudo rm -rf /var/root/Library/Preferences/ManagedInstalls.plist
-sudo rm -rf /var/root/Library/munki_manifests
-sudo defaults read /var/root/Library/Preferences/ManagedInstalls.plist
+rm -f /Library/LaunchDaemons/com.googlecode.munki.*
+rm -f /Library/LaunchAgents/com.googlecode.munki.*
+rm -rf "/Library/Managed Installs"
+rm -rf /usr/local/munki
+rm /etc/paths.d/munki
+rm -rf /var/root/Library/Preferences/ManagedInstalls.plist
+rm -rf /var/root/Library/munki_manifests
+defaults read /var/root/Library/Preferences/ManagedInstalls.plist
 
-sudo pkgutil --forget com.googlecode.munki.admin
-sudo pkgutil --forget com.googlecode.munki.app
-sudo pkgutil --forget com.googlecode.munki.core
-sudo pkgutil --forget com.googlecode.munki.launchd
-sudo pkgutil --forget com.yp.YP_Munki_Additions_FULL
+pkgutil --forget com.googlecode.munki.admin
+pkgutil --forget com.googlecode.munki.app
+pkgutil --forget com.googlecode.munki.core
+pkgutil --forget com.googlecode.munki.launchd
+pkgutil --forget com.yp.YP_Munki_Additions_FULL
